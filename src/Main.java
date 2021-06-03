e9ccc2e309028e14c707e3410fbefdb830513960
David Birtles <dbirtles@hotmail.com>
2021-06-03 13:30:45 +0200

    More tests

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.pydawan.terminal.Color;
 import com.pydawan.terminal.TerminalPrinter;
 
 public class Main {
-
+	
 public static void main(String ... args)  {
         try(TerminalPrinter term = new TerminalPrinter(System.out)){
             term.bold(true);
